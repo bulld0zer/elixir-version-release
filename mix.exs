809f8fd8e8a19799db07504fa0c534d1c7768cd7
Aleksandr <aleksandr@heathmont.net>
2021-06-07 17:13:28 +0300

    [version_release] version {{version}}

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule VersionRelease.MixProject do
   def project do
     [
       app: :version_release,
-      version: "0.1.1-rc.1",
+      version: "0.1.1-rc.2",
       elixir: "~> 1.8",
       description: "Project version and changelog managment",
       package: [
